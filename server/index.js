de3620b61f2264476dd7105a87e6ee2589acbc48
mazam5 <mohammedazam651@gmail.com>
2025-04-14 23:12:09 +0530

    fix backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 import cors from "cors";
 import { configDotenv } from "dotenv";
 import express from "express";
-import {
-  instagramCallback,
-  instagramLogin,
-} from "./controllers/authController.js";
 
 configDotenv();
 
@@ -17,36 +13,6 @@ const port = PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
-app.get("/api/privacy-policy", (req, res) => {
-  res.sendFile("privacy-policy.html", { root: "./public" });
-});
-
-app.get("/api/webhook", (req, res) => {
-  const hubMode = req.query["hub.mode"];
-  const hubChallenge = req.query["hub.challenge"];
-  const hubVerifyToken = req.query["hub.verify_token"];
-  if (hubChallenge && hubMode) {
-    if (
-      hubMode === "subscribe" &&
-      hubVerifyToken === process.env.WEBHOOK_VERIFY_TOKEN
-    ) {
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(hubChallenge);
-    } else {
-      res.sendStatus(403);
-    }
-  }
-});
-
-app.post("/api/webhook", (req, res) => {
-  console.log("Webhook received:", req.body);
-  res.sendStatus(200);
-});
-
-// app.use("/api", authRoutes);
-app.get("/api/auth/instagram", instagramLogin);
-app.get("/api/auth/instagram/callback", instagramCallback);
 
 app.listen(PORT, () => {
   if (process.env.NODE_ENV !== "production") {
